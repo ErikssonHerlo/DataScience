52faf4e27aa11815999156a5073d96769dbd7b6b
Hernandez Lopez Eriksson Jose <erikssonhernandez201830459@cunoc.edu.gt>
2022-07-04 02:23:15 -0600

    Linear Regression Completed

--- pages/1_Regresion_Lineal.py
+++ pages/1_Regresion_Lineal.py
@@ -50,15 +50,16 @@ if(uploadFile is not None):
         Elija las variables que se utilizarán para el análisis de regresión líneal """)
     st.markdown("#### Variable Independiente (X)")
     var_X = st.selectbox("Por favor elija una opción", df.keys(), key="variableX")
-    st.markdown("##### Variable Dependiente (Y)")
+    st.markdown("#### Variable Dependiente (Y)")
     var_Y = st.selectbox("Por favor elija una opción", df.keys(), key="variableY")
-    st.markdown("##### Predicción")
+    st.markdown("#### Valor de la Predicción")
+    predValue = st.number_input("Ingrese el valor de la Predicción")
     st.markdown("#### Colores de la Gráfica")
     col1, col2 = st.columns(2)
     with col1:
         colorPoints = st.color_picker('Elije un Color para los Puntos de la gráfica','#EF280F')
     with col2:
-        colorLine = st.color_picker('Elije un Color para la Recta de la gráfica', '#024A86')
+        colorLine = st.color_picker('Elije un Color para la Tendencia de la gráfica', '#024A86')
     #Transformar Data a Array
     x = np.asarray(df[var_X]).reshape(-1, 1)
     # x = np.asarray(df[var_x])
@@ -69,6 +70,7 @@ if(uploadFile is not None):
     regr.fit(x, y)
     y_pred = regr.predict(x)
     r2 = r2_score(y, y_pred)
+    predict = regr.predict([[predValue]])
 
     #Graficacion
     fig = plt.figure()
@@ -88,6 +90,33 @@ if(uploadFile is not None):
         #image = Image.open("linearRegression.png")
         #st.image(image, caption = "Linear Regression")
         st.pyplot(fig)
+        st.markdown("#### Datos de la Gráfica")
+
+        col1, col2= st.columns(2)
+        
+        pendiente =  float(regr.coef_)
+        indicadorPendiente = "+ Positiva" if pendiente>=0 else "- Negativa" 
+        col1.metric("Pendiente", pendiente, indicadorPendiente)
+        
+        intercepto = float(regr.intercept_)
+        indicadorIntercepto = "+ Positivo" if intercepto>=0 else "- Negativo"
+        col2.metric("Intercepto", intercepto, indicadorIntercepto)
+        
+        col3, col4 = st.columns(2)
+        col3.metric("Coeficiente de Determinación",r2)
+        col4.metric("Error Cuadrático", mean_squared_error(y, y_pred))
+        
+        st.subheader("Función de la Tendencia")
+        
+        operador = "+ " if intercepto>=0 else "" 
+        st.latex(f"f(x)={pendiente}X {operador}{intercepto}")
+        st.subheader("Predicción")
+        indicadorPrediccion = "+ Positiva" if predict>=0 else "- Negativa"
+        st.metric(f"El valor de la predicción para {predValue} es de: ",predict, indicadorPrediccion)
+
+
+
+
         
     
 
@@ -100,3 +129,12 @@ else:
 st.sidebar.title("Indice")
 st.sidebar.markdown("### [Carga del Archivo](#carga-del-archivo)")
 st.sidebar.markdown("- [Contenido del Archivo](#contenido-del-archivo)")
+st.sidebar.markdown("### [Parametrización](#parametrizaci-n)")
+st.sidebar.markdown("- [Variable Indepentiente (X)](#variable-independiente-x)")
+st.sidebar.markdown("- [Variable Depentiente (Y)](#variable-dependiente-y)")
+st.sidebar.markdown("- [Valor de la Predicción](#valor-de-la-predicci-n)")
+st.sidebar.markdown("- [Colores de la Gráfica](#colores-de-la-gr-fica)")
+st.sidebar.markdown("### [Graficación](#graficaci-n)")
+st.sidebar.markdown("- [Datos de la Gráfica](#datos-de-la-gr-fica)")
+st.sidebar.markdown("### [Función de la Tendencia](#funci-n-de-la-tendencia)")
+st.sidebar.markdown("### [Predicción](#predicci-n)")
